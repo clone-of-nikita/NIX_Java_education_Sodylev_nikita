985307e87ef602f26dcf0d8020afb315acffbaba
clone-of-nikita <sodylevnikita@gmail.com>
2021-10-12 14:38:16 +0300

    ChatBot 1 stage

---
+++ day_4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Date;
+public class day_4 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Hello!My name is Igor");
+        System.out.printf("I was created in " + date);
+    }
+}
