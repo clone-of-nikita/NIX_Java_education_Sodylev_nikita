a60e60d2f26848bc31551697eaa1fc3e3d338fa5
clone-of-nikita <sodylevnikita@gmail.com>
2021-10-12 14:51:53 +0300

    ChatBot 2 stage

--- day_4.java
+++ day_4.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 import java.util.Date;
 public class day_4 {
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         Date date = new Date();
         System.out.println("Hello!My name is Igor");
         System.out.printf("I was created in " + date);
+
+        System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + ".");
     }
 }
